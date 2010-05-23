2113b83db17f880b47a461e1ea264e34b9aad7b6
Lane Liles <lane.liles@gmail.com>
2010-05-23 11:00:27 -0400

    set open browser command

--- lane/init.el
+++ lane/init.el
@@ -6,3 +6,7 @@
 
 ;; remove hl-mode hook
 (remove-hook 'coding-hook 'turn-on-hl-line-mode)
+
+;; Set this to whatever browser you use
+;; (setq browse-url-browser-function 'browse-url-firefox)
+(setq browse-url-browser-function 'browse-default-macosx-browser)
