d65ce5df775d1181b92f7c0b0be2cbdf375fc43c
Lane Liles <lane.liles@gmail.com>
2010-08-11 20:58:31 -0400

    set mysql output to vertical, added function to mark entire buffer as rectangle

--- lane/init.el
+++ lane/init.el
@@ -3,6 +3,7 @@
 
 ;; set misc settings
 (setq visible-bell nil)
+(setq column-number-mode t)
 
 ;; remove hl-mode hook
 (remove-hook 'coding-hook 'turn-on-hl-line-mode)
@@ -13,9 +14,51 @@
 ;;(setq browse-url-browser-function 'browse-url-generic
 ;;      browse-url-generic-program "~/src/conkeror/conkeror")
 
-
 ;; org mode experimentation
 (setq org-log-done t)
 
 ;; allow narrowing
 (put 'narrow-to-region 'disabled nil)
+
+
+;; set mysql client output to vertical instead of table
+(setq sql-mysql-options (list "-E"))
+
+;; custom functions
+(defun longest-line-length ()
+  "Returns the length of the longest line in the current buffer.
+If called interactively, a message is printed in the echo area,
+otherwise just the length is returned."
+  (interactive)
+  (let (longest-line)
+    (save-excursion
+      (goto-char (point-min))
+      (end-of-line)
+      (setq longest-line (current-column))
+      (while (< (forward-line 1) 1)
+        (end-of-line)
+        (if (> (current-column) longest-line)
+            (setq longest-line (current-column))))
+      (if (called-interactively-p)
+          (message "Longest line length: %d" longest-line)))
+    longest-line))
+
+(defun mark-buffer-as-rectangle ()
+  "Puts mark at beginning of current buffer and moves point
+to the last line and column needed to capture all lines in 
+the buffer as a rectangle. Appends spaces to the last line
+as needed to match the length of the longest line."
+  (interactive)
+  (let ((longest-line (longest-line-length)))
+    (goto-char (point-min))
+    (push-mark)
+    (goto-char (point-max))
+    (while (< (current-column) (+ longest-line 1))
+      (insert " "))))
+
+;; TODO
+(defun kill-rectangle-save ()
+  "Save the rectangle as if killed with kill-rectangle, but
+don't delete it.")
+
+
