a57dece8761414e70b24a66bb978f804f84fcb6f
Lane Liles <lane.liles@gmail.com>
2010-08-11 22:04:40 -0400

    another cursor color change

--- lane/color-theme-dark-laptop.el
+++ lane/color-theme-dark-laptop.el
@@ -9,7 +9,7 @@ Includes custom, fl, font-lock, gnus, message, widget."
      ((background-color . "black")
       (background-mode . dark)
       (border-color . "black")
-      (cursor-color . "yellow")
+      (cursor-color . "dark green")
       (foreground-color . "white")
       (mouse-color . "sienna1"))
      ((gnus-mouse-face . highlight)
