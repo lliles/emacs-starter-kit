e4b37d860bcb04f7635b99b1544f75c612b7331f
Lane Liles <lane.liles@gmail.com>
2010-12-01 13:59:22 -0500

    removed durendal and rvm - installed thru elpa

--- lane/zzz-init.el
+++ lane/zzz-init.el
@@ -1,15 +1,6 @@
 ;; final init file after all others in directory have been loaded
 
 ;; symlinked files for easier git updating
-;; durendal.el  -> ../../durendal/durendal.el
-;; rvm.el       -> ../../rvm.el/rvm.el
 
 ;; load dark laptop color theme
 (color-theme-dark-laptop)
-
-;; enable durendal
-(durendal-enable)
-
-;; enable rvm.el
-(rvm-use-default)
-
