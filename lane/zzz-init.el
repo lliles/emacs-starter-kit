4fd38836a989407e80a388f202e90a665265771c
Lane Liles <lane.liles@gmail.com>
2011-01-31 12:23:56 -0500

    enable durendal

--- lane/zzz-init.el
+++ lane/zzz-init.el
@@ -1,6 +1,3 @@
 ;; final init file after all others in directory have been loaded
-
-;; symlinked files for easier git updating
-
-;; load dark laptop color theme
 (color-theme-dark-laptop)
+(durendal-enable)
