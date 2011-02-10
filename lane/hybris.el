3a59a114113b7973eab9fc2aab963b63eb3bdb3e
Lane Liles <lane.liles@gmail.com>
2011-02-10 18:28:37 -0500

    add functions for expanding and collapsing impex

--- lane/hybris.el
+++ lane/hybris.el
@@ -10,3 +10,17 @@
     (goto-char (point-min))
     (while (re-search-forward "	" nil t)
       (replace-match ";"))))
+
+(defun expand-impex (start end)
+  "Aligns the impex along semicolons"
+  (interactive "r")
+  (align-regexp start end "\\(\\s-*\\);" 1 1 t))
+
+(defun collapse-impex (start end)
+  "Collapse the impex - opposite of expand-impex"
+  (interactive "r")
+  (save-restriction
+    (narrow-to-region start end)
+    (goto-char (point-min))
+    (while (re-search-forward "\\s-*;" nil t)
+      (replace-match ";"))))
